1a10996804cbfbae5647f6557ac45567dc9ba728
Sayak Bhattacharya <sayak6237@gmail.com>
2023-08-30 21:21:41 +0530

    Added push notifications and new messaging widget

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,15 +1,36 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:wavelink/widgets/chat_messages.dart';
 import 'package:wavelink/widgets/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.onInverseSurface,
         appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
           title: const Text('Chats'),
           actions: [
             // Signing out
--- lib/widgets/message_bubble.dart
+++ lib/widgets/message_bubble.dart
@@ -93,7 +93,7 @@ class MessageBubble extends StatelessWidget {
                   Container(
                     decoration: BoxDecoration(
                       color: isMe
-                          ? Colors.grey[300]
+                          ? theme.colorScheme.primary.withAlpha(10)
                           : theme.colorScheme.secondary.withAlpha(200),
                       // Only show the message bubble's "speaking edge" if first in
                       // the chain.
